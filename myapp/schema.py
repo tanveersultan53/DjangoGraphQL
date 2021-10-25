02c7550d26ce07cccfe381e6b93399764358df62
Muhammad Tanveer Sultan <engr.tanveersultan53@gmail.com>
2021-10-25 16:36:39 +0500

    Building GraphQL APIs in Django with Graphene

--- myapp/schema.py
+++ myapp/schema.py
@@ -11,10 +11,59 @@ class AuthorType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     all_authors = graphene.List(AuthorType)
+    single_author = graphene.Field(AuthorType, id=graphene.Int())
 
     @graphene.resolve_only_args
     def resolve_all_authors(self):
         return Author.objects.filter()
 
+    def resolve_single_author(self, info, id):
+        return Author.objects.get(pk=id)
 
-schema = graphene.Schema(query=Query)
+
+class AuthorMutation(graphene.Mutation):
+    class Arguments:
+        name = graphene.String(required=True)
+        biodata = graphene.String(required=True)
+
+    author = graphene.Field(AuthorType)
+
+    @classmethod
+    def mutate(cls, root, info, name, biodata):
+        author = Author(name=name, biodata=biodata)
+        author.save()
+        return AuthorMutation(author=author)
+class AuthorUpdate(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        name = graphene.String(required=True)
+        biodata = graphene.String(required=True)
+
+    author = graphene.Field(AuthorType)
+
+    @classmethod
+    def mutate(cls, root, info, id,name, biodata):
+        author = Author.objects.get(pk=id)
+        author.name = name
+        author.biodata = biodata
+        author.save()
+        return AuthorMutation(author=author)
+
+class AuthorDelete(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+    author = graphene.Field(AuthorType)
+    @classmethod
+    def mutate(cls, root, info, id):
+        author = Author.objects.get(id=id)
+        author.delete()
+        return "Delete Data Successfully!"
+
+
+class Mutation(graphene.ObjectType):
+    add_author = AuthorMutation.Field()
+    author_delete = AuthorDelete.Field()
+    update_author = AuthorUpdate.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
